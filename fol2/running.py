ac9b86f223e06e2557eed7eca15900db9e54df55
ManishSingh98 <manish.singh@thoughtworks.com>
2020-08-03 00:46:50 +0530

    fix: path is made generic for fol1

--- fol2/running.py
+++ fol2/running.py
@@ -1,6 +1,6 @@
 import sys
 
 if __name__=='__main__':
-    sys.path.insert(0, '/Users/manishluthyagi/Desktop/Courses/circleci/python-flask/fol1')
+    sys.path.insert(0, './fol1')
     import function
     function.printing_hello_world()
